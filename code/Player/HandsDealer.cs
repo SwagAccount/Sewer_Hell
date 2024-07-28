791ec31c978175423fcdb023c7f1482c11cfe093
thehumbleonion <160753124+SwagAccount@users.noreply.github.com>
2024-07-29 02:01:52 +0700

    Initial commit

---
+++ code/Player/HandsDealer.cs
@@ -0,0 +1,11 @@
+using Sandbox;
+
+public sealed class HandsDealer : Component
+{
+	[Property] public GameObject leftHand {get;set;}
+	[Property] public GameObject rightHand {get;set;}
+	protected override void OnUpdate()
+	{
+
+	}
+}
