e1c3387e8f178abc6e9e02694b642aa37bf9bb41
thehumbleonion <160753124+SwagAccount@users.noreply.github.com>
2024-08-07 17:37:53 +0700

    Hitbox Issue

--- code/SboxVrraycastStupidAndBroken.cs
+++ code/SboxVrraycastStupidAndBroken.cs
@@ -4,7 +4,9 @@ public sealed class SboxVrraycastStupidAndBroken : Component
 {
 	protected override void OnUpdate()
 	{
-		var ray = Scene.Trace.Ray(Transform.Position, Transform.Position + Transform.World.Forward * 100).UseHitboxes().Run();
+		var ray = Scene.Trace.Ray(Transform.Position, Transform.Position + Transform.World.Forward * 10000).UseHitboxes().Run();
+		Gizmo.Draw.Line(Transform.Position, Transform.Position + Transform.World.Forward * 10000);
 		if(ray.Hit) Log.Info(ray.GameObject.Name);
+		//Transform.Rotation = Transform.Rotation * new Angles(0,Time.Delta*50,0).ToRotation();
 	}
 }
